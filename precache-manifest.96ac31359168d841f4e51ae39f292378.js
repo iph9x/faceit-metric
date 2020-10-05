eddd8fd2ff7dc79e7e14d966c2b10025148eee0e
iph9x <iphnx9@gmail.com>
2020-10-05 22:40:16 +0700

    Updates

---
+++ precache-manifest.96ac31359168d841f4e51ae39f292378.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c589c5246c984300553bbd8ee3c9bf",
+    "url": "/faceit-metric/index.html"
+  },
+  {
+    "revision": "0b83b2f4cf02391cdebf",
+    "url": "/faceit-metric/static/css/main.3c47953e.chunk.css"
+  },
+  {
+    "revision": "cfa0ff89484a56ad1512",
+    "url": "/faceit-metric/static/js/2.1db55f6d.chunk.js"
+  },
+  {
+    "revision": "eb825d785f7a33be4f13471daee73ff2",
+    "url": "/faceit-metric/static/js/2.1db55f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b83b2f4cf02391cdebf",
+    "url": "/faceit-metric/static/js/main.587c9ee9.chunk.js"
+  },
+  {
+    "revision": "731a377354d8a87f0d74",
+    "url": "/faceit-metric/static/js/runtime-main.f2b938bc.js"
+  }
+]);
