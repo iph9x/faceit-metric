f02ccbb96825030cec77449043ae9313d884c7ab
iph9x <iphnx9@gmail.com>
2020-10-06 11:44:35 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/faceit-metric/precache-manifest.f499c26323ceeae0e50ba6e913bfa84a.js"
+  "/faceit-metric/precache-manifest.1bb78f4d76efdcf417c396a7f813f35f.js"
 );
 
 self.addEventListener('message', (event) => {
