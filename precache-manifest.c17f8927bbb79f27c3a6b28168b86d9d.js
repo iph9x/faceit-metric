08f4581cdc4ff1cf459d61375e4b992b2607f649
iph9x <iphnx9@gmail.com>
2021-08-13 01:16:45 +0700

    Updates

---
+++ precache-manifest.c17f8927bbb79f27c3a6b28168b86d9d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce16e8f7b2fba84023026bb0584b975",
+    "url": "/faceit-metric/index.html"
+  },
+  {
+    "revision": "a184598d19a4b0a10097",
+    "url": "/faceit-metric/static/css/main.bc60a207.chunk.css"
+  },
+  {
+    "revision": "b773ead315c184430401",
+    "url": "/faceit-metric/static/js/2.83df162b.chunk.js"
+  },
+  {
+    "revision": "eb825d785f7a33be4f13471daee73ff2",
+    "url": "/faceit-metric/static/js/2.83df162b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a184598d19a4b0a10097",
+    "url": "/faceit-metric/static/js/main.b92ea839.chunk.js"
+  },
+  {
+    "revision": "731a377354d8a87f0d74",
+    "url": "/faceit-metric/static/js/runtime-main.f2b938bc.js"
+  },
+  {
+    "revision": "8e035cd059184ff58d47d2c15090d570",
+    "url": "/faceit-metric/static/media/de_nuke.8e035cd0.jpg"
+  },
+  {
+    "revision": "cf3156e15006d0d3ea5354438451f404",
+    "url": "/faceit-metric/static/media/karambit.cf3156e1.svg"
+  },
+  {
+    "revision": "9ece5b48963bbc96309220952cda38aa",
+    "url": "/faceit-metric/static/media/roboto-bold.9ece5b48.ttf"
+  },
+  {
+    "revision": "a62b1119c7c06ce6ff05650c20e26266",
+    "url": "/faceit-metric/static/media/roboto-bold.a62b1119.woff"
+  },
+  {
+    "revision": "e991503992965b360d495a8d134cd10c",
+    "url": "/faceit-metric/static/media/roboto-bold.e9915039.svg"
+  },
+  {
+    "revision": "352eef08e3b4f81901d3132030b74a90",
+    "url": "/faceit-metric/static/media/roboto-light.352eef08.woff"
+  },
+  {
+    "revision": "6090d256d88dcd7f0244eaa4a3eafbba",
+    "url": "/faceit-metric/static/media/roboto-light.6090d256.ttf"
+  },
+  {
+    "revision": "ebed571b3d79179aa8965a7400666f19",
+    "url": "/faceit-metric/static/media/roboto-light.ebed571b.svg"
+  },
+  {
+    "revision": "4469fc422b92517dbc4b3ee294ec3a17",
+    "url": "/faceit-metric/static/media/roboto-regular.4469fc42.woff"
+  },
+  {
+    "revision": "93e89ff3f8843bb67ee57a6683890da9",
+    "url": "/faceit-metric/static/media/roboto-regular.93e89ff3.svg"
+  },
+  {
+    "revision": "f36638c2135b71e5a623dca52b611173",
+    "url": "/faceit-metric/static/media/roboto-regular.f36638c2.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9ff156b482f566163646f574d1cf908c.js"
+  "/faceit-metric/precache-manifest.c17f8927bbb79f27c3a6b28168b86d9d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/faceit-metric/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
