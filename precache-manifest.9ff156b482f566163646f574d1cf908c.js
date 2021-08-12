86b5c7c32785082ee1f27bb03123388c31bd2921
iph9x <iphnx9@gmail.com>
2021-08-13 01:23:15 +0700

    Updates

---
+++ precache-manifest.9ff156b482f566163646f574d1cf908c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d2dbf76b85cb12c834e40b3326bc41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8debf01a77cc5b70a2ff",
+    "url": "/static/css/main.b10f71af.chunk.css"
+  },
+  {
+    "revision": "610a60607e5a337a4e67",
+    "url": "/static/js/2.6764b69b.chunk.js"
+  },
+  {
+    "revision": "eb825d785f7a33be4f13471daee73ff2",
+    "url": "/static/js/2.6764b69b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8debf01a77cc5b70a2ff",
+    "url": "/static/js/main.c6c12b98.chunk.js"
+  },
+  {
+    "revision": "7d89e230f54ce00ad297",
+    "url": "/static/js/runtime-main.a43d2cea.js"
+  },
+  {
+    "revision": "8e035cd059184ff58d47d2c15090d570",
+    "url": "/static/media/de_nuke.8e035cd0.jpg"
+  },
+  {
+    "revision": "cf3156e15006d0d3ea5354438451f404",
+    "url": "/static/media/karambit.cf3156e1.svg"
+  },
+  {
+    "revision": "9ece5b48963bbc96309220952cda38aa",
+    "url": "/static/media/roboto-bold.9ece5b48.ttf"
+  },
+  {
+    "revision": "a62b1119c7c06ce6ff05650c20e26266",
+    "url": "/static/media/roboto-bold.a62b1119.woff"
+  },
+  {
+    "revision": "e991503992965b360d495a8d134cd10c",
+    "url": "/static/media/roboto-bold.e9915039.svg"
+  },
+  {
+    "revision": "352eef08e3b4f81901d3132030b74a90",
+    "url": "/static/media/roboto-light.352eef08.woff"
+  },
+  {
+    "revision": "6090d256d88dcd7f0244eaa4a3eafbba",
+    "url": "/static/media/roboto-light.6090d256.ttf"
+  },
+  {
+    "revision": "ebed571b3d79179aa8965a7400666f19",
+    "url": "/static/media/roboto-light.ebed571b.svg"
+  },
+  {
+    "revision": "4469fc422b92517dbc4b3ee294ec3a17",
+    "url": "/static/media/roboto-regular.4469fc42.woff"
+  },
+  {
+    "revision": "93e89ff3f8843bb67ee57a6683890da9",
+    "url": "/static/media/roboto-regular.93e89ff3.svg"
+  },
+  {
+    "revision": "f36638c2135b71e5a623dca52b611173",
+    "url": "/static/media/roboto-regular.f36638c2.ttf"
+  }
+]);
